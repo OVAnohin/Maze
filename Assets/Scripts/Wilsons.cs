10ba4060379df7b4f5eec75f1ff5f892b05b2421
Oleg Anohin <oanohin@outlook.com>
2021-05-16 22:44:24 +0400

    Fix

--- Assets/Scripts/Wilsons.cs
+++ Assets/Scripts/Wilsons.cs
@@ -15,8 +15,15 @@ namespace Maze
             int z = Random.Range(1, Depth - 1);
             Map[x, z] = (int)MazeElement.StartPosition;
 
+            int loop = 0;
             while (GetAvailableCells() > 1)
+            {
+                loop++;
                 RandomWalk();
+
+                if (loop > 5000)
+                    break;
+            }
         }
 
         private int GetAvailableCells()
@@ -24,13 +31,9 @@ namespace Maze
             _notUsed.Clear();
 
             for (int z = 1; z < Depth - 1; z++)
-            {
                 for (int x = 1; x < Width - 1; x++)
-                {
                     if (CountSquareMazeNeighbours(x, z) == (int)MazeElement.Corridor)
                         _notUsed.Add(new MapLocation(x, z));
-                }
-            }
 
             return _notUsed.Count;
         }
@@ -51,8 +54,6 @@ namespace Maze
         private void RandomWalk()
         {
             List<MapLocation> inWalk = new List<MapLocation>();
-            //int startX = Random.Range(1, Width - 1);
-            //int startZ = Random.Range(1, Depth - 1);
             int randomStartIndex = Random.Range(0, _notUsed.Count);
 
             int startX = _notUsed[randomStartIndex].X;
@@ -65,7 +66,11 @@ namespace Maze
             int loop = 0;
             while (startX > 0 && startX < Width - 1 && startZ > 0 && startZ < Depth - 1 && loop < 5000 && !validPath)
             {
+                loop++;
+
                 Map[startX, startZ] = (int)MazeElement.Corridor;
+                if (CountSquareMazeNeighbours(startX, startZ) > 1)
+                    break;
 
                 int random = Random.Range(0, _directions.Count);
                 int nextX = startX + _directions[random].X;
@@ -76,9 +81,8 @@ namespace Maze
                     startZ = nextZ;
                     inWalk.Add(new MapLocation(startX, startZ));
                 }
-                validPath = CountSquareMazeNeighbours(startX, startZ) == 1;
 
-                loop++;
+                validPath = CountSquareMazeNeighbours(startX, startZ) == 1;
             }
 
             if (validPath)
